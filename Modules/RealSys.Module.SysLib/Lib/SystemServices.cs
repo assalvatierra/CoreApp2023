745b29e2c70f28b7bde9a83963295d6651fde9fb
Abel S. Salvatierra <assalvatierra@gmail.com>
2023-03-10 20:26:41 +0000

    updates

---
+++ Modules/RealSys.Module.SysLib/Lib/SystemServices.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RealSys.CoreLib.Models.SysDB;
+using RealSys.CoreLib.Interfaces;
+using RealSys.CoreLib.Interfaces.System;
+
+namespace RealSys.Modules.SysLib
+{
+    public class SystemServices: ISystemServices
+    {
+        private readonly SysDBContext context;
+        public SystemServices(SysDBContext _context) { 
+            this.context = _context;
+        }
+
+        public virtual IQueryable<SysService> getServices(int _userId)
+        {
+            return this.context.SysServices;
+        }
+    }
+}
---
+++ RealSys.CoreLib/Interfaces/System/ISystemServices.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RealSys.CoreLib.Models.SysDB;
+
+namespace RealSys.CoreLib.Interfaces.System
+{
+    public interface ISystemServices
+    {
+        public IQueryable<SysService> getServices(int _userId);
+    }
+}
