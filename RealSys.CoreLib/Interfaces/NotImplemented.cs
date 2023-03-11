67ee08261e5a1f0afac443b9b0de4acb27eeffb1
Abel Salvatierra <assalvatierra@gmail.com>
2023-03-11 09:00:17 -0800

    cleanup

---
+++ RealSys.CoreLib/Interfaces/NotImplemented.cs
@@ -0,0 +1,22 @@
+﻿using RealSys.CoreLib.Interfaces;
+using RealSys.CoreLib.Models;
+
+namespace NotImplementedModules
+{
+    //public class SupplierServices : ISupplierService
+    //{
+    //    Supplier ISupplierService.GetSupplier(int Id)
+    //    {
+    //        throw new NotImplementedException();
+    //    }
+
+    //    IQueryable<Supplier> ISupplierService.GetSuppliers()
+    //    {
+    //        throw new NotImplementedException();
+    //    }
+    //    public int UpdateSupplier(JobsV1.Models.Supplier supplier)
+    //    {
+    //        throw new NotImplementedException();
+    //    }
+    //}
+}
