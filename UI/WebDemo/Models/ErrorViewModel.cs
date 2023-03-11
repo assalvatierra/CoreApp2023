bfa9e633a6285e6f4639697f5075f91943b5eee0
Abel S. Salvatierra <assalvatierra@gmail.com>
2023-03-11 01:09:25 +0000

    structure update

--- UI/WebDemo/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace WebDemo.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
