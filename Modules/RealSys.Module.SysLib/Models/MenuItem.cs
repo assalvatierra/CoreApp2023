5581725e29519efb839da0fce5733cc2c2772e25
jahdiel <jahdielsvillosa@gmail.com>
2023-04-19 19:03:13 +0800

    Fix Menu and Submenu Items

--- Modules/RealSys.Module.SysLib/Models/MenuItem.cs
+++ Modules/RealSys.Module.SysLib/Models/MenuItem.cs
@@ -12,6 +12,18 @@ namespace RealSys.Modules.SysLib.Models
         public string? MenuName { get; set; }
         public int OrderNo { get; set; }
         public string? Route { get; set; }
+        public List<SubMenuItem> SubMenuItems { get; set; }
+
+    }
+
+    public class SubMenuItem
+    {
+        public int Id { get; set; }
+        public int MainMenuId { get; set; }
+        public string? MenuName { get; set; }
+        public int OrderNo { get; set; }
+        public string? Route { get; set; }
 
     }
 }
+
