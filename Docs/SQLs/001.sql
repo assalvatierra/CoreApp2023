3fd40ba30a7c864c8ba0abc3bf80a17e688b8ef8
Abel Salvatierra <assalvatierra@gmail.com>
2023-04-30 23:24:04 -0700

    Update 001.sql

--- Docs/SQLs/001.sql
+++ Docs/SQLs/001.sql
@@ -1,2 +1,12 @@
 select * from InvItems;
 select * from SupplierInvItems;
+
+select * from InvItemCats;
+select * from InvItems;
+
+select * from SupplierItems;
+
+select * from SupplierTypes;
+select * from InvItemCats;
+
+
